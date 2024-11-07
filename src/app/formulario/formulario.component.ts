79ff308483654b877d4bd5c893d5164718f3f954
Matheus Moreira <micaelmodesto11@gmail.com>
2024-11-06 23:34:29 -0300

    FEATURE - Ajustando erros após testes de integração com API

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-formulario',
@@ -7,6 +9,7 @@ import { Component } from '@angular/core';
 })
 export class FormularioComponent {
   etapa = 1;
+  isLoading = false;
   dados: any = {
     nome: '',
     cpf: '',
@@ -15,11 +18,14 @@ export class FormularioComponent {
     valorFinanciamento: null,
     prazo: null,
     rendaMensal: null,
-    patrimonio: null
+    patrimonio: null,
+    token: '' // Adiciona o campo de token
   };
 
+  constructor(private http: HttpClient) {}
+
   avancarEtapa() {
-    if (this.etapa < 3) {
+    if (this.etapa < 4) { // Aumenta o número de etapas para incluir a verificação de token
       this.etapa++;
     }
   }
@@ -32,9 +38,73 @@ export class FormularioComponent {
 
   onSubmit(form: any) {
     if (form.valid) {
-      console.log("Formulário enviado com sucesso!", this.dados);
+      this.http.post(environment.apiUrl+'/lead', this.dados)
+        .subscribe({
+          next: (response) => {
+            console.log("Formulário enviado com sucesso!");
+            alert("Dados enviados com sucesso!");
+            this.avancarEtapa(); // Avança para a etapa de verificação de token
+            this.isLoading = false;
+          },
+          error: (error) => {
+            console.error("Erro ao enviar formulário:", error);
+            alert("Houve um erro ao enviar os dados. Tente novamente. \n Erro: " + error);
+            this.isLoading = false;
+          }
+        });
     } else {
       console.log("Formulário inválido.");
     }
+  }
+
+  verificarToken() {
+    // Aqui você faz a chamada para verificar o token
+    this.http.get(environment.apiUrl + '/token/'+this.dados.email, {})
+      .subscribe({
+        // Retorna o token que foi enviado para o email
+        next: (response) => {
+          if(this.dados.token = response){
+            this.confirmaToken();
+            console.log("Token verificado com sucesso!", response);
+            alert("Token verificado com sucesso!");
+            this.avancarEtapa(); // Aqui você pode avançar para a próxima tela após a verificação do token
+          }else{
+            alert("Token inválido! Tente novamente ou reenvie!");
+          }
+          
+        },
+        error: (error) => {
+          console.error("Erro ao verificar token:", error);
+          alert("Token inválido. Tente novamente.");
+        }
+      });
+  }
+
+  enviarNovamenteToken() {
+    // Função para reenviar o token, se necessário
+    this.http.post(environment.apiUrl + '/token/send/'+this.dados.email, {})
+      .subscribe({
+        next: (response) => {
+          console.log("Token reenviado com sucesso!", response);
+          alert("Token reenviado para o seu email.");
+        },
+        error: (error) => {
+          console.error("Erro ao reenviar token:", error);
+          alert("Houve um erro ao reenviar o token. Tente novamente.");
+        }
+      });
+  }
+
+  confirmaToken(){
+    this.http.put(environment.apiUrl + '/token/confirm/' + this.dados.email, {})
+      .subscribe({
+        next: (response) => {
+          console.log("Confirmando envio de token, ok!");
+        },
+        error: (error) => {
+          alert("Erro ao confirmar que o token foi correto!:" + error);
+          console.error("Erro ao confirmar que o token foi correto!:", error);
+        }
+    })
   }
 }
